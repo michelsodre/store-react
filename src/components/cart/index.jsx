e2f235204f9218a476795c46bf25a453d25016bf
Michel <106924948+michelsodre@users.noreply.github.com>
2025-01-15 18:11:31 -0300

    Update: Cart Total Price

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { selectCartTotal } from "../../redux/cart/cart-selector";
 //Styles
 import * as Styles from "./styles";
 //Components
@@ -7,12 +8,13 @@ import CartItem from "../cart-item";
 const Cart = ({ isOpen, setIsOpen }) => {
   const handleEscapeAreaClick = () => setIsOpen(false);
   const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
-
+  const CartTotal = useSelector(selectCartTotal);
   return (
     <Styles.CartContainer isOpen={isOpen}>
       <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
       <Styles.CartContent>
         <Styles.CartTitle>Your Cart</Styles.CartTitle>
+        <p>R$ {CartTotal}</p>
         {products.map((product) => (
           <CartItem key={product.id} product={product} />
         ))}
